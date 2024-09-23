11e3363b32d282acb5df8f0a5c30b4a7a9608b28
feoxy8 <nandoxu17@gmail.com>
2024-09-23 16:33:20 +0800

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Final
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN: Final = '7324851083:AAEOWTNYBsFG2ZrVNEakCuXG-xRDwaQH6Wg'
+BOT_USERNAME: Final = '@iSplitMoneyBot'
+
+async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await update.message.reply_text('Hello! Thanks for chatting with me!')
+
+if __name__ == '__main__':
+  app = Application.builder().token(TOKEN).build()
+
+  # Commands
+  app.add_handler(CommandHandler('start', start_command))
+
+  app.run_polling(poll_interval=3)
